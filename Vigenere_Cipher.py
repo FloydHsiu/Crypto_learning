1239ae2a12fd75e699b509ca1dcd3fa44d7d41e3
張修齊 <Floyd@Floyd-MacBook-Pro.local>
2016-01-28 12:08:34 +0800

    Vigenere cipher decrypt

--- Vigenere_Cipher.py
+++ Vigenere_Cipher.py
@@ -1,19 +1,34 @@
 import tkinter as tk
 from tkinter import filedialog 
 
+# key is a word, ex: "ABC"
 def Encrypt(key, plain):
 	count = 0
 	key_len = len(key)
 	key = key.upper()
 	plain = plain.upper()
-	p = ""
+	cipher = ""
 	for i in plain:
 		if i.isalpha():
 			shift = ord(key[count]) - 65
 			temp = (ord(i)-65+shift)%26 + 65
-			p = p + chr(temp)
+			cipher = cipher + chr(temp)
 			count = (count+1)%key_len
-	return p
+	return cipher
+
+def Decrypt(key, cipher):
+	count = 0
+	key_len = len(key)
+	key = key.upper()
+	cipher = cipher.upper()
+	plain = ''
+	for i in cipher:
+		if i.isalpha():
+			shift = ord(key[count]) - 65
+			temp = (ord(i)-65-shift+26)%26 + 65
+			plain = plain + chr(temp)
+			count = (count+1)%key_len
+	return plain
 
 root = tk.Tk()
 root.withdraw()
