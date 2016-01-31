480890aaa527c5f747c4bbb1f4cf9d2b84cdd3d9
張修齊 <Floyd@1-160-119-48.dynamic.hinet.net>
2016-01-31 11:15:32 +0800

    Create directory Classical Encryption

--- Classical Encryption/Autokey_Cipher.py
+++ Classical Encryption/Autokey_Cipher.py
@@ -9,7 +9,7 @@ def Encrypt(key, plain):
 	cipher = ''
 	autokey = ''
 	for i in plain:
-		if plain.isalpha():
+		if i.isalpha():
 			autokey = autokey + i
 			shift = ord(key[count]) - 65
 			temp = (ord(i)-65+shift)%26 + 65
@@ -19,6 +19,26 @@ def Encrypt(key, plain):
 				key = autokey
 				autokey = ''
 	return cipher
+
+def Decrypt(key, cipher):
+	count = 0
+	key_len = len(key)
+	key = key.upper()
+	cipher = cipher.upper()
+	plain = ''
+	autokey = ''
+	for i in cipher:
+		if i.isalpha():
+			shift = ord(key[count]) - 65
+			temp = (ord(i)-65-shift+26)%26 + 65
+			plain = plain + chr(temp)
+			autokey = autokey + chr(temp)
+			count = (count+1)%key_len
+			if count== 0:
+				key = autokey
+				autokey = ''
+	return plain
+
 
 root = tk.Tk()
 root.withdraw()
