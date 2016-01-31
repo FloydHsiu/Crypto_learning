4ee62491f6256040ca636ff7397a8c01193dff8a
張修齊 <Floyd@1-160-119-48.dynamic.hinet.net>
2016-01-31 15:41:03 +0800

    add comment

--- Classical Encryption/Caesar_Cipher.py
+++ Classical Encryption/Caesar_Cipher.py
@@ -1,42 +1,34 @@
-def Encrypt(key, p):
-	if p.isalpha():
-		if p.islower():
-			result = (ord(p)-97+key)%26 + 97
-			return (chr(result))
-		else:
-			result = (ord(p)-65+key)%26 + 65
-			return (chr(result))
-	else:
-		return p
+#Caesar Cipher - Shift and monoalphabetic cipher
+def Encrypt(key, plain):
+        cipher = ''
+        for p in plain:
+                if p.isalpha():
+                        if p.islower():
+                                result = (ord(p)-97+key)%26 + 97
+                                cipher = cipher + (chr(result))
+                        else:
+                                result = (ord(p)-65+key)%26 + 65
+                                cipher = cipher + (chr(result))
+                else:
+                        cipher = cipher + p
+        return cipher
 	
 def Decrypt(key, p):
-        if p.isalpha():
-                if p.islower():
-                        result = (ord(p)-97+26-key)%26 + 97
-                        return (chr(result))
+        cipher = ''
+        for p in plain:
+                if p.isalpha():
+                        if p.islower():
+                                result = (ord(p)-97+26-key)%26 + 97
+                                cipher = cipher + (chr(result))
+                        else:
+                                result = (ord(p)-65+26-key)%26 + 65
+                                cipher = cipher + (chr(result))
                 else:
-                        result = (ord(p)-65+26-key)%26 + 65
-                        return (chr(result))
-        else:
-                return p
+                        cipher = cipher + p
+        return cipher
  
 #ord(p): a function which can change str to unicode(number)
 #chr(result): a function which can change unicode(number) to str
-
-'''      
-f = open('/Users/Floyd/Desktop/test', 'r')
-a = f.read()
-wf = open('/Users/Floyd/Desktop/crypto', 'w')
-for i in a:
-	wf.write(Encrypt(1, i))
-wf.close()
-f.close()
-
-f = open('/Users/Floyd/Desktop/crypto', 'r')
-a = f.read()
-for i in a:
-        print(Decrypt(1, i), end="")
-f.close()'''
 
 print ("Input the plain text:")
 p = input()
@@ -44,8 +36,7 @@ print ("input the key:")
 k = input()
 if k.isdecimal():
         key = int(k)
-        for i in p:
-                print(Encrypt(key, i), end="")
+        print (Encrypt(key, p))
 else:
         print("You enter wrong key!")
 
