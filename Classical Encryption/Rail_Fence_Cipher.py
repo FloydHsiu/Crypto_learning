4c7b076e98c355064d3c6a8f67a099dc2516a29c
張修齊 <Floyd@Floyd-MacBook-Pro.local>
2016-02-02 21:00:37 +0800

    Explanation: rail fence cipher

---
+++ Classical Encryption/Rail_Fence_Cipher.py
@@ -0,0 +1,9 @@
+#Rail Fence Cipher - Transposition ciphers
+''' 
+EX: Key:4 3 1 2 5 6 7
+		a t t a c k p
+		o s t p o n e 
+		d u n t i l t 
+		w o a m x y z 
+Cipher = TTNAAPTMTSUOAODWCOIXKNLYPETZ
+'''
